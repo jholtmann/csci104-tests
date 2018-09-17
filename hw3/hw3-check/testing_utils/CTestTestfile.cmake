80327903498e85b43ef682274aaeaf881af63d7f
Matthew Treadwell <Treadwell@inventati.org>
2018-09-17 12:55:59 -0700

    garbage file deletion

--- hw3/hw3-check/testing_utils/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/jholt/cs104/hw-holtmann/hw3/hw3-check/testing_utils
-# Build directory: /home/jholt/cs104/hw-holtmann/hw3/hw3-check/testing_utils
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("kwsys")
