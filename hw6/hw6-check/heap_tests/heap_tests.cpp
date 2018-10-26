329cc3d031180b3d56193f0e56ac946cff455a4e
Jonathan Holtmann <jonathan@holtmann.xyz>
2018-10-25 21:49:40 -0700

    Parameterized everything

--- hw6/hw6-check/heap_tests/heap_tests.cpp
+++ hw6/hw6-check/heap_tests/heap_tests.cpp
@@ -6,68 +6,65 @@
 #include <vector>
 #include <algorithm>
 #include <utility>
+#include <cstdlib>
 
 #include <cassert>
 
 using namespace std;
 
 /*** Test Fixtures ***/
+
 class MinHeapNum : public ::testing::Test {
 protected:
   MinHeap<int>* mh;
 
   void SetUp(int d) {
-    mh = new MinHeap<int>(d);
+	mh = new MinHeap<int>(d);
   }
 
   virtual void TearDown() {
-    delete mh;
+	delete mh;
   }
 };
 
-class MinHeapSort : public ::testing::Test {
+class MinHeapNumParam : public MinHeapNum, public ::testing::WithParamInterface<int> {
+protected:
+};
+
+class MinHeapSort : public ::testing::Test, public ::testing::WithParamInterface<int> {
 protected:
   MinHeap<int>* mh;
   size_t array_size;
 
   void SetUp(size_t array_size) {
-    this->array_size = array_size;
+	this->array_size = array_size;
   }
 
-  void runRange(size_t begin, size_t end, size_t skip = 1) {
-    assert(begin >= 2 && begin < end);
+  void TearDown() {
+	mh = new MinHeap<int>(GetParam());
+	vector<int> vec = makeRandomNumberVector(array_size, 0, 2147483646, 12345, true);
+	vector<int> vec2 = makeRandomNumberVector(array_size, 0, 2147483646, 12345, false);
 
-    for (size_t i = begin; i < end; i += skip) {
-      cout << "Running with k=" << i << endl;
-      run(i);
-    }
-  }
+	vector<pair<int, int>> res;
 
-  void run(int d) {
-    mh = new MinHeap<int>(d);
-    vector<int> vec = makeRandomNumberVector(array_size, 0, 2147483646, 12345, true);
-    vector<int> vec2 = makeRandomNumberVector(array_size, 0, 2147483646, 12345, false);
+	for (size_t i = 0; i < vec.size(); i++) {
+	  mh->add(vec[i], vec2[i]);
+	  res.push_back(make_pair(vec[i], vec2[i]));
+	}
 
-    vector<pair<int, int>> res;
+	// https://stackoverflow.com/a/279878
+	std::sort(res.begin(), res.end(), [](const std::pair<int,int> &left, const std::pair<int,int> &right) {
+	  return left.second < right.second;
+	});
 
-    for (size_t i = 0; i < vec.size(); i++) {
-      mh->add(vec[i], vec2[i]);
-      res.push_back(make_pair(vec[i], vec2[i]));
-    }
+	for (size_t i = 0; i < res.size(); i++) {
+	  EXPECT_EQ(mh->peek(), res[i].first);
+	  mh->remove();
+	}
 
-    // https://stackoverflow.com/a/279878
-    std::sort(res.begin(), res.end(), [](const std::pair<int,int> &left, const std::pair<int,int> &right) {
-      return left.second < right.second;
-    });
+	EXPECT_TRUE(mh->isEmpty());
 
-    for (size_t i = 0; i < res.size(); i++) {
-      EXPECT_EQ(mh->peek(), res[i].first);
-      mh->remove();
-    }
-
-    EXPECT_TRUE(mh->isEmpty());
-
-    delete mh;
+	delete mh;
   }
 };
 
@@ -76,39 +73,55 @@ protected:
   MinHeap<string>* mh;
 
   void SetUp(int d) {
-    mh = new MinHeap<string>(d);
+	mh = new MinHeap<string>(d);
   }
 
   virtual void TearDown() {
-    delete mh;
+	delete mh;
   }
 };
 
-TEST_F (MinHeapNum, TwoAryAdd) {
-  SetUp(2);
+/*** Value Paramaterized Tests ***/
+// Test MinHeapNumParam tests with a d-value from 2-20
+INSTANTIATE_TEST_CASE_P(MinHeapNumMultiSmall, MinHeapNumParam, ::testing::Range(2, 21));
+
+TEST_P (MinHeapNumParam, add) {
+  SetUp(GetParam());
   mh->add(420, 1);
 }
 
-TEST_F (MinHeapNum, ThreeAryAdd) {
-  SetUp(3);
-  mh->add(420, 1);
-}
-
-TEST_F (MinHeapNum, peek) {
-  SetUp(2);
+TEST_P (MinHeapNumParam, peek) {
+  SetUp(GetParam());
   mh->add(420, 1);
   EXPECT_EQ(mh->peek(), 420);
 }
 
-TEST_F (MinHeapNum, peekTwoItems) {
-  SetUp(2);
+TEST_P (MinHeapNumParam, rm) {
+  SetUp(GetParam());
+  mh->add(420, 1);
+  mh->add(911, 2);
+  mh->remove();
+  EXPECT_EQ(mh->peek(), 911);
+}
+
+TEST_P (MinHeapNumParam, empty) {
+  SetUp(GetParam());
+  EXPECT_TRUE(mh->isEmpty());
+  mh->add(420, 1);
+  mh->remove();
+  EXPECT_TRUE(mh->isEmpty());
+}
+
+
+TEST_P (MinHeapNumParam, peekTwoItems) {
+  SetUp(GetParam());
   mh->add(111, 1);
   mh->add(120, 2);
   EXPECT_EQ(mh->peek(), 111);
 }
 
-TEST_F (MinHeapNum, peekFiveItems) {
-  SetUp(2);
+TEST_P (MinHeapNumParam, peekFiveItems) {
+  SetUp(GetParam());
   mh->add(100, 2);
   mh->add(400, 7);
   mh->add(300, 3);
@@ -133,31 +146,32 @@ TEST_F (MinHeapNum, peekFiveItems) {
   EXPECT_TRUE(mh->isEmpty());
 }
 
-TEST_F (MinHeapNum, rm) {
-  SetUp(2);
-  mh->add(420, 1);
-  mh->add(911, 2);
-  mh->remove();
-
-  EXPECT_EQ(mh->peek(), 911);
+TEST_P (MinHeapNumParam, PeekHundredItems) {
+  SetUp(GetParam());
+for (int b = 0; b < 100; b++) {
+  mh->add(b, 100 - b);
+  EXPECT_EQ(mh->peek(), b);
+}
 }
 
-TEST_F (MinHeapNum, empty) {
-  SetUp(2);
-  EXPECT_TRUE(mh->isEmpty());
-
-  mh->add(420, 1);
-  mh->remove();
-
+TEST_P (MinHeapNumParam, FillToTheBrimEmpty) {
+  SetUp(GetParam());
+  srand(1);
+  for (int i = 0; i < 10000; i++) {
+	mh->add(i, 10000 - i);
+  }
+  for (int i = 0; i < 10000; i++) {
+  	mh->remove();
+  }
   EXPECT_TRUE(mh->isEmpty());
 }
 
 TEST_F (MinHeapNum, EmptyRemove) {
   SetUp(2);
   try {
-    mh->remove();
+	mh->remove();
   } catch (exception& e) {
-    return;
+	return;
   }
   FAIL();
 }
@@ -165,9 +179,9 @@ TEST_F (MinHeapNum, EmptyRemove) {
 TEST_F (MinHeapNum, EmptyPeek) {
   SetUp(2);
   try {
-    mh->peek();
+	mh->peek();
   } catch (exception& e) {
-    return;
+	return;
   }
   FAIL();
 }
@@ -202,37 +216,20 @@ TEST_F (MinHeapString, empty) {
   EXPECT_TRUE(mh->isEmpty());
 }
 
+
+INSTANTIATE_TEST_CASE_P(MinHeapSortVaryingD, MinHeapSort, ::testing::Range(2, 104));
+
 /* Perform heap sort with 100 elements and k-ary heaps in steps of 1 */
-TEST_F (MinHeapSort, HeapSort100) {
+TEST_P (MinHeapSort, HeapSort100) {
   SetUp(100);
-  runRange(2, 100);
+}
+
+/* Perform heap sort with 10000 elements and k-ary heaps in steps of 3 */
+TEST_P (MinHeapSort, HeapSort543) {
+  SetUp(543);
 }
 
 /* Perform heap sort with 1000 elements and k-ary heaps in steps of 3 */
-TEST_F (MinHeapSort, HeapSort1000) {
+TEST_P (MinHeapSort, HeapSort1000) {
   SetUp(1000);
-  runRange(2, 100, 3);
-}
-
-/* Perform  100 add operations on a d-ary heap for 2 <= d <= 100 */
-TEST (MinHeapNary, DAryAdd) {
-  MinHeap<int>* mh;
-  for (int i = 2; i <= 100; i++) {
-    mh = new MinHeap<int>(i);
-    mh->add(i, i);
-    delete mh;
-  }
-}
-
-/* Perform 100 add and peek operations on a d-ary heap for 2 <= d <= 100 */
-TEST (MinHeapNary, DAryPeek) {
-  MinHeap<int>* mh;
-  for (int i = 2; i <= 100; i++) {
-    mh = new MinHeap<int>(i);
-    for (int b = i; b < 20*i; b++) {
-      mh->add(b, 20*i - b);
-      EXPECT_EQ(mh->peek(), b);
-    }
-    delete mh;
-  }
 }
