63762143e7a461ae10ac4f18ab35a0e8713d3d05
Matthew Treadwell <Treadwell@inventati.org>
2018-10-25 18:43:19 -0700

    first attempt at value paramaterized tests

--- hw6/hw6-check/heap_tests/heap_tests.cpp
+++ hw6/hw6-check/heap_tests/heap_tests.cpp
@@ -20,6 +20,21 @@ protected:
   }
 };
 
+class MinHeapNumParam : public ::testing::TestWithParam<const int> {
+protected:
+  MinHeap<int>* mh;
+  int d;
+
+  void SetUp(int d) {
+    mh = new MinHeap<int>(d);
+  }
+
+  virtual void TearDown() {
+    delete mh;
+  }
+};
+
+
 class MinHeapString : public ::testing::Test {
 protected:
   MinHeap<string>* mh;
@@ -33,6 +48,7 @@ protected:
   }
 };
 
+/*** Basic Tests ***/
 TEST_F (MinHeapNum, add) {
   SetUp(2);
   mh->add(420, 1);
@@ -92,3 +108,39 @@ TEST_F (MinHeapString, empty) {
 
   EXPECT_TRUE(mh->isEmpty());
 }
+
+/*** Value Paramaterized Tests ***/
+// Test MinHeapNumParam tests with a d-value from 2-20
+INSTANTIATE_TEST_CASE_P(MinHeapNumMultiSmall, MinHeapNumParam, ::testing::Range(2, 21))
+
+TEST_P (MinHeapNumParam, add) {
+  SetUp(GetParam());
+  mh->add(420, 1);
+}
+
+TEST_P (MinHeapNumParam, peek) {
+  SetUp(GetParam());
+  mh->add(420, 1);
+  EXPECT_EQ(mh->peek(), 420);
+}
+
+TEST_P (MinHeapNumParam, rm) {
+  SetUp(GetParam());
+  mh->add(420, 1);
+  mh->add(911, 2);
+  mh->remove();
+
+  EXPECT_EQ(mh->peek(), 911);
+}
+
+TEST_P (MinHeapNumParam, empty) {
+  SetUp(GetParam());
+  EXPECT_TRUE(mh->isEmpty());
+
+  mh->add(420, 1);
+  mh->remove();
+
+  EXPECT_TRUE(mh->isEmpty());
+}
+
+
